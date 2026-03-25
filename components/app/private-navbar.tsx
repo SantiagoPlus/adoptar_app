95fc75817e4894ada7574adcc4ac221b2282e60d
SantiagoPlus <buffonesantiago@argeaplus.com>
2026-03-24 21:27:13 -0300

    Update private-navbar.tsx

--- components/app/private-navbar.tsx
+++ components/app/private-navbar.tsx
@@ -5,8 +5,9 @@ export default function PrivateNavbar() {
   return (
     <header className="sticky top-0 z-50 border-b border-white/10 bg-black/80 backdrop-blur">
       <div className="mx-auto flex max-w-6xl items-center justify-between px-6 py-4">
-        <Link href="/publicaciones" className="text-2xl font-semibold text-white">
-          Adopta App
+        <Link href="/" className="text-2xl font-semibold text-white">
+        Adopta App
+        </Link>
         </Link>
 
         <nav className="flex items-center gap-3">
