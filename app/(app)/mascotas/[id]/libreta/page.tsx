874cb860e250b4ff6ff66f731a90d57b3e482f05
SantiagoPlus <buffonesantiago@argeaplus.com>
2026-04-12 17:32:51 -0300

    Add loading state for LibretaPage with Suspense

--- app/(app)/mascotas/[id]/libreta/page.tsx
+++ app/(app)/mascotas/[id]/libreta/page.tsx
@@ -1,5 +1,32 @@
+import { Suspense } from "react";
 import { LibretaTab } from "./libreta-tab";
+import { getMascotaLibreta } from "@/lib/server/mascotas-libreta";
 
-export default function LibretaPage() {
-  return <LibretaTab />;
+type PageProps = {
+  params: Promise<{
+    id: string;
+  }>;
+};
+
+function LibretaFallback() {
+  return (
+    <section className="rounded-[22px] border border-white/10 bg-[#080808] p-6">
+      <div className="h-56 rounded-[20px] bg-white/5" />
+    </section>
+  );
+}
+
+async function LibretaPageContent({ params }: PageProps) {
+  const { id } = await params;
+  const libreta = await getMascotaLibreta(id);
+
+  return <LibretaTab libreta={libreta} />;
+}
+
+export default function LibretaPage(props: PageProps) {
+  return (
+    <Suspense fallback={<LibretaFallback />}>
+      <LibretaPageContent {...props} />
+    </Suspense>
+  );
 }
