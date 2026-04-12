188786e65b7687ae2bcf18feb9d0883045718b7d
SantiagoPlus <buffonesantiago@argeaplus.com>
2026-04-12 01:38:48 -0300

    Enhance LibretaTab with additional props

--- app/(app)/mascotas/[id]/libreta/libreta-tab.tsx
+++ app/(app)/mascotas/[id]/libreta/libreta-tab.tsx
@@ -6,5 +6,12 @@ import { useMascotaModuleData } from "../mascota-module-provider";
 export function LibretaTab() {
   const { mascota, libreta } = useMascotaModuleData();
 
-  return <LibretaContent idMascota={mascota.id_mascota} libreta={libreta} />;
+  return (
+    <LibretaContent
+      idMascota={mascota.id_mascota}
+      libreta={libreta}
+      ownerDisplayName={mascota.owner_display_name}
+      qrMicrochip={mascota.qr_microchip}
+    />
+  );
 }
