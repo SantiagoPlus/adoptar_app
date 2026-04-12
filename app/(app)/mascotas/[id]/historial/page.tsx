5c1df2e7027deb5fab7b14cd097ea211856bb302
SantiagoPlus <buffonesantiago@argeaplus.com>
2026-04-12 01:30:09 -0300

    Refactor HistorialPage to use HistorialTab component

--- app/(app)/mascotas/[id]/historial/page.tsx
+++ app/(app)/mascotas/[id]/historial/page.tsx
@@ -1,15 +1,5 @@
-import { HistorialContent } from "./historial-content";
-import { getMascotaHistorial } from "@/lib/server/mascotas-historial";
+import { HistorialTab } from "./historial-tab";
 
-type PageProps = {
-  params: Promise<{
-    id: string;
-  }>;
-};
-
-export default async function HistorialPage(props: PageProps) {
-  const { id: idMascota } = await props.params;
-  const historial = await getMascotaHistorial(idMascota);
-
-  return <HistorialContent idMascota={idMascota} historial={historial} />;
+export default function HistorialPage() {
+  return <HistorialTab />;
 }
